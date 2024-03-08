0e9975b052672e20bc2055e77cf8fe59b30e342a
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-08 16:23:50 +0100

    hi

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
+from time import sleep
 
 options = FirefoxOptions()
 options.add_argument("--headless")
 browser = webdriver.Firefox(options=options)
 browser.get('http://win-1ics5jffm16.dev.local:9080/navigator/')
-browser.implicitly_wait(10)
+sleep(5)
 if(browser.title=="IBM Content Navigator"):
     print ("yes!!!")
 else:
