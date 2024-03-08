a42fead3b678ea38f0493c17d7d5396d7dacc22d
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-08 15:42:58 +0100

    hi

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 options = FirefoxOptions()
 options.add_argument("--headless")
 browser = webdriver.Firefox(options=options)
-browser.get('http://google.com/')
+browser.get('http://192.168.247.167:9443/')
 
-if(browser.title=="Goole"):
+if(browser.title=="No target servlet"):
     print ("yes!!!")
 else:
     print ("no!!!")
