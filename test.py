1d401020d294cb28978239f9d4714c2c844534cb
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-08 16:12:06 +0100

    hi

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ options = FirefoxOptions()
 options.add_argument("--headless")
 browser = webdriver.Firefox(options=options)
 browser.get('http://win-1ics5jffm16.dev.local:9080/navigator/')
-
+browser.implicitly_wait(10)
 if(browser.title=="No target servlet"):
     print ("yes!!!")
 else:
