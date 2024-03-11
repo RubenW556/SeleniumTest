5db9c723543e0c9f30d55930e13005d2d5353eb3
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-11 15:38:58 +0100

    hi

--- test_example.py
+++ test_example.py
@@ -1,25 +1,26 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 from time import sleep
+browser = 0
 
-
-def test_hi():
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    global browser
     options = FirefoxOptions()
     options.add_argument("--headless")
     browser = webdriver.Firefox(options=options)
     browser.get('http://win-1ics5jffm16.dev.local:9080/navigator/')
+    browser.implicitly_wait(2)
+
+def test_hi():
     sleep(3)
     assert browser.title == "IBM Content Navigator"
     browser.quit()
 
 
 def test_hi2():
-    options = FirefoxOptions()
-    options.add_argument("--headless")
-    browser = webdriver.Firefox(options=options)
-    browser.get('http://win-1ics5jffm16.dev.local:9080/navigator/')
-    browser.implicitly_wait(2)
     actual = browser.find_element(By.XPATH, "// *[ @ id = \"ecm_widget_layout_NavigatorMainLayout_0_LoginPane\"] / div[1] / table / tbody / tr / td[2] / div[1]").text
-    expected = "Welcome to IBM Contet Navigator"
+    expected = "Welcome to IBM Content Navigator"
     assert actual == expected
